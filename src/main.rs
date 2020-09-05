20cd13c2053c80230aa66843977ce66a36bf5cda
Mason Sipe <masonsipe0513@gmail.com>
2020-09-05 02:50:14 -0500

    [RST] reset to a new template. Last project was unsuccessful. This will make it a command-line based tool instead of a framework. [CLI] Command line tool now takes arguments to make actions happen.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,14 @@
-mod messages;
-mod options;
-mod db;
-extern crate pbr;
+use structopt::StructOpt;
 
-
-fn main()
-{
-    messages::banner();
-    use std::io::{stdin,stdout,Write};
-    let mut s=String::new();
-    print!("Enter an option: ");
-    let _=stdout().flush();
-    stdin().read_line(&mut s).expect("Did not enter a correct string");
-    if let Some('\n')=s.chars().next_back() {
-        s.pop();
-    }
-    if let Some('\r')=s.chars().next_back() {
-        s.pop();
-    } else if s == "1" {
-            messages::wordlist_choices();
-            options::option_1();
-        } else if s == "2"{
-            println!("Under Development.");
-        } else if s == "3" {
-            messages::attack_menu();
-            //actions toward selection in options to add here.
-            crate::main();
-        } else if s == "0" {
-            messages::epilogue();
-            std::process::exit(1);
-        } else if s == "" {
-                println!(":/");
-                crate::main();
-            } else {
-                println!("{} did not match any valid protocols.",s);
-                crate::main();
-            }
-        
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+fn main() {
+    let args = Cli::from_args();
 }
