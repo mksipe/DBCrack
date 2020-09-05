b5103765dca4a2f55a498f24b1babdf452ceaee3
Mason Sipe <masonsipe0513@gmail.com>
2020-09-05 03:14:39 -0500

    [FIX] Fixed Argumentitive issues [NEW] added new arguments [INF] staging is complete for development further into the project.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
-use structopt::StructOpt;
+use clap::{Arg, App};
 
-/// Search for a pattern in a file and display the lines that contain it.
-#[derive(StructOpt)]
-struct Cli {
-    /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
-}
 fn main() {
-    let args = Cli::from_args();
+    let matches = App::new("DBCrack")
+        .version("2.0.1")
+        .author("Mason Sipe <m-sipe@protonmail.com>")
+        .about("A Reverse Lookup Password Cracker")
+        .arg(Arg::with_name("wordlist")
+                 .short("w")
+                 .long("import-wordlist")
+                 .takes_value(true)
+                 .help("Import a wordlist location to the database."))
+        .arg(Arg::with_name("batch")
+                 .short("b")
+                 .long("--batch")
+                 .takes_value(false)
+                 .help("First imports terms into the database, Then converts plaintext terms in the database to their hashed form."))
+        .arg(Arg::with_name("single hash")
+                 .short("a")
+                 .long("--attack")
+                 .takes_value(true)
+                 .help("Attacks a single hash from the STDIN."))
+        .arg(Arg::with_name("PWDUMP")
+                 .short("A")
+                 .long("--attack-file")
+                 .takes_value(true)
+                 .help("Attacks a list of hashes."))
+        .get_matches();
+
+    let myfile = matches.value_of("file").unwrap_or("input.txt");
+    println!("The file passed is: {}", myfile);
+
 }
