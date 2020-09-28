1d3748576e879446f5a4ed3d7611ecfd7a0147bf
Mason Sipe <m-sipe@protonmail.com>
2020-09-28 18:02:30 -0500

    [FIX] removed the struct that required unstable blocks to be used. Helps allocate more resources instead of using them in an unsafe environment.

--- src/wordlist.rs
+++ src/wordlist.rs
@@ -37,20 +37,21 @@ pub fn main(){
     check_db()
     
 }
-static mut COUNT: i64 = 0;
+
 
 pub fn add(value: &str) {
+    let mut COUNT: i64 = 0;
     let conn = Connection::open("db.sqlite3").unwrap();
     let file = File::open(value).unwrap();
     let buff = BufReader::new(&file);
     for (index, _line) in buff.lines().enumerate(){
         let _ifile = File::open(value).expect("Unable to open file");
-        unsafe{COUNT = ((index + 1)).try_into().unwrap();}
+        COUNT = ((index + 1)).try_into().unwrap();
     }
-    unsafe {
-        let countstr = COUNT.to_string();
-        let counter: &str = &countstr;
-        conn.execute("INSERT INTO wordlists(PATH, WORDS) VALUES (?1, ?2)", &[&value, &counter]).unwrap();
-        conn.execute("delete from wordlists where rowid NOT IN (SELECT MIN(rowid) from wordlists group by PATH)", NO_PARAMS).unwrap(); // remove duplicate paths.
-    }
+    
+    let countstr = COUNT.to_string();
+    let counter: &str = &countstr;
+    conn.execute("INSERT INTO wordlists(PATH, WORDS) VALUES (?1, ?2)", &[&value, &counter]).unwrap();
+    conn.execute("delete from wordlists where rowid NOT IN (SELECT MIN(rowid) from wordlists group by PATH)", NO_PARAMS).unwrap(); // remove duplicate paths.
+    
 }
