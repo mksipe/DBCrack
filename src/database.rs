72a1612cb6fbc7feb13eaa3c0f4f97caa11e5503
Mason Sipe <m-sipe@protonmail.com>
2020-11-01 16:49:03 -0600

    Test with Jolly's repo. If it works that is pog. If not. Not pog.

---
+++ src/database.rs
@@ -0,0 +1,13 @@
+
+
+fn create_schema() {
+    //DB entry goes here
+}
+
+fn delete_schema() {
+    //opposite from create_schema
+}
+
+pub fn main() {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,6 @@
 //    You should have received a copy of the GNU General Public License
 //    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
-mod readiness;
-mod wordlist;
-mod crypto;
-
-
 #[macro_use]
 extern crate clap;
 use clap::App;
@@ -24,46 +18,15 @@ use clap::App;
 fn main() {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from_yaml(yaml).get_matches();
-    println!("{}", "\nArguments Selected:\n");
-    // This will check to see what is already selected to be displayed. 
-    if matches.is_present("wordlist") {
-        println!("{}", "[ ENABLED  ] Wordlist addition");
-    } else {
-        println!("{}", "[ DISABLED ] Wordlist addition")
-    }
-    if matches.is_present("batch") {
-        println!("{}", "[ ENABLED  ] Batch database");
-    } else {
-        println!("{}", "[ DISABLED ] Batch database")
-    }
-    if matches.is_present("single hash") {
-        println!("{}", "[ ENABLED  ] Single hash attack");
-    } else {
-        println!("{}", "[ DISABLED ] Single hash attack")
-    }
-    if matches.is_present("PWDUMP") {
-        println!("{}", "[ ENABLED  ] PWDUMP attack");
-    } else {
-        println!("{}", "[ DISABLED ] PWDUMP attack")
-    }
-    println!("{}", "\nReadiness Information:\n");
-    readiness::db_exists();
-    readiness::locked();
     //This is where the actual application of the arguments take place.
     if matches.is_present("wordlist") {
-        wordlist::main();
-        let file: &str  = matches.value_of("wordlist").unwrap();
-        wordlist::add(file);
     }
     if matches.is_present("batch") {
-        wordlist::batch();
-        crypto::main();
     }
     if matches.is_present("single hash") {
     } 
     if matches.is_present("PWDUMP") {
     } 
     if matches.is_present("show_wordlist") {
-        wordlist::show_wordlists();
     }
 }
