a9fa5393d1238866a94dfd31b04acc3b2856ac5e
Mason Sipe <masonsipe0513@gmail.com>
2020-11-05 01:12:16 -0500

    v3.0.8 [FIX]   Changed the issue where hashmaps flipped data when inputting a string into the function hash()         Removed the function to call hashmaps altogether in order to make this fix persistent. This was accomplished by using a struct instead.

--- src/exfil/src/lib.rs
+++ src/exfil/src/lib.rs
@@ -1,14 +1,23 @@
 use hex;
-use std::collections::HashMap;
 
 pub fn hash(x: String) {
-    let mut calculated = HashMap::new();
-    let md2 = md2(x.clone());
-    calculated.insert(String::from("ASCII:"), x);
-    calculated.insert(String::from("MD2:"), md2);
-    for (a, b) in &calculated {
-        println!("| {} {}", a,b);
+    #[derive(Debug)]
+    struct Data {
+        ascii   : String,
+        md2     : String,
     };
+
+    let md2 = md2(x.clone());
+    
+    let mut stag = Data {
+        ascii: String::from("test"),
+        md2: String::from("A1"),
+    };
+
+    stag.ascii = String::from(x.clone());
+    stag.md2 = String::from(md2);
+
+    println!("{:?}", stag);
 }
 
 
